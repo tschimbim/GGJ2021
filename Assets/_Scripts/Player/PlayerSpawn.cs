6c078c3b43cd77403463186c512f4f7eb6a939ad
tschimbim <tschimbim.twitch@gmail.com>
2021-01-30 15:58:18 +0100

    changed player spawn

--- Assets/_Scripts/Player/PlayerSpawn.cs
+++ Assets/_Scripts/Player/PlayerSpawn.cs
@@ -3,29 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Photon.Pun;
+using System.Linq;
 
 public class PlayerSpawn : MonoBehaviour
 {
     public GameObject PlayerPrefab;
-    public bool host = false;
+    public int mySpawnIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        NetworkManager networkManager = (NetworkManager) FindObjectOfType(typeof(NetworkManager));
+        NetworkManager networkManager = (NetworkManager)FindObjectOfType(typeof(NetworkManager));
         if (networkManager.IsInRoom())
-        {
             CreatePlayer();
-        }
     }
 
     void CreatePlayer()
     {
-        NetworkManager networkManager = (NetworkManager) FindObjectOfType(typeof(NetworkManager));
-        if (networkManager.IsHost() == host)
-        {
+        if (PhotonNetwork.PlayerList.Length <= mySpawnIndex)
+            return;
+
+        if (PhotonNetwork.PlayerList[mySpawnIndex].IsLocal)
             PhotonNetwork.Instantiate("MPPrefabs/" + PlayerPrefab.name, transform.position, Quaternion.identity, 0);
-        }
+
         Destroy(gameObject);
     }
 }
