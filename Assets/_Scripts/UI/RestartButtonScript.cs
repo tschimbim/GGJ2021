ad0a2453af1a114ce00066dc655e441467da322f
tschimbim <tschimbim.twitch@gmail.com>
2021-01-31 15:09:34 +0100

    added restart button and improved lobby

--- Assets/_Scripts/UI/RestartButtonScript.cs
+++ Assets/_Scripts/UI/RestartButtonScript.cs
@@ -1,11 +1,29 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RestartButtonScript : MonoBehaviour
 {
+    private bool startedRoomSwitch = false;
+
+    private void Start()
+    {
+        gameObject.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
     public void RestartGame()
     {
-        GameManager.instance.StartGame();
+        startedRoomSwitch = true;
+
+        Transition transition = (Transition)FindObjectOfType(typeof(Transition));
+        transition.StartTransition(false, 0.6f);
+
+        Invoke(nameof(DoChangeLevel), 0.6f);
+    }
+
+    public void DoChangeLevel()
+    {
+        PhotonNetwork.LoadLevel(0); // Main menu
     }
 }
